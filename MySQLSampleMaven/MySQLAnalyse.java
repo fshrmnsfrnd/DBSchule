8ee2bb2a1cfbf45aef6bb4d1d15d483a0fe924da
fshrmnsfrnd <fshrmnsfrnd@web.de>
2025-11-10 10:42:46 +0100

    DBVereinClient angefangen

---
+++ MySQLSampleMaven/MySQLAnalyse.java
@@ -0,0 +1,106 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+
+/**
+ * Code-Analyse
+ * 
+ * @author (ml) 
+ * @version (09.11.2025)
+ */
+package blogic;
+
+import java.sql.*;
+
+public class App
+{
+    public static void main(String args[])
+    {
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        String sql= null;
+        int mId = 0;
+        int sportId = 0;
+
+
+        try
+        {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbVerein","root","kennwort1");
+            //Start Transaction
+            con.setAutoCommit(false);
+            stmt = con.createStatement();
+           
+
+            //neues Mitglied
+            sql ="Insert into mitglied (Vorname,Nachname,Geburtsdatum,Geschlecht, Strasse,Ort_id,Tel)";
+            sql = sql + " VALUES ('Max','Mustermann','1981-01-01','m','Bahnhofstr. 10',21,'11111');";
+            stmt.execute(sql,Statement.RETURN_GENERATED_KEYS);
+            rs = stmt.getGeneratedKeys();
+            rs.next();
+            mId = rs.getInt(1);
+            
+            //neue Sportart
+            sql ="Insert into Sportart (sportart,beitrag)";
+            sql = sql + " VALUES ('Rugby','120.00');";
+            stmt.execute(sql,Statement.RETURN_GENERATED_KEYS); 
+            rs = stmt.getGeneratedKeys();
+            rs.next();
+            sportId = rs.getInt(1);
+            
+            //neuer Link
+            sql ="Insert into Link_Mitglied_Sportart (m_id,sport_id)";
+            sql = sql + " VALUES ("+ mId +","+sportId+");";
+            stmt.execute(sql);         
+            
+            
+            
+            
+            //query new Mitglied
+            sql ="Select m.*,s.*";
+            sql = sql + " from mitglied m, link_mitglied_sportart l, sportart s"; 
+            sql = sql + " WHERE m.m_id = l.m_id AND l.Sport_id = s.sport_id";
+            sql = sql + " AND m.m_id = " + mId;
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next())
+            {
+                System.out.print(rs.getInt("m.m_id")+ ";");
+                System.out.print(rs.getString("Vorname")+ ";");
+                System.out.print(rs.getString("Nachname")+ ";");
+                System.out.print(rs.getString("s.Sportart")+ ";");
+                System.out.print(rs.getString("s.beitrag")+ ";");                
+                System.out.println("");
+            }
+            //end transaction
+            con.commit();
+        }
+        catch (SQLException e)
+        {
+            try
+            {           
+                con.rollback();
+                System.out.println(e.getMessage());     
+            }
+            catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());     
+            }        
+        }
+        finally
+        {
+            try
+            {
+                con.close();
+            }
+            catch (SQLException ex)
+            {
+                System.out.println(ex.getMessage());     
+            }
+        }
+
+    }
+}
+
---
+++ SQLJava/DbVereinClient/src/main/java/ts/dbvereinclient/SportartController.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ts.dbvereinclient;
+
+/**
+ *
+ * @author ml
+ */
+import javafx.scene.control.Label;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SportartController {
+    @FXML
+    Label  lblStatusLeft;
+    
+    //Membervariablen
+    private Stage stage;
+    
+
+    //Getter und Setter
+    public Stage getStage() {
+        return stage;
+    }
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+    
+    public void close()
+    {
+        try
+        {
+            // FXML-Loader erzeugen und Layout auswählen
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(App.class.getResource("main.fxml"));
+            // Root der neuen Scene speichern
+            Parent root = loader.load();
+            
+            //aktuellen Controller von main.xml holen und stage übergeben
+            MainController controller = loader.getController();
+            controller.setStage(stage);
+            
+            //Neue Szene erzeugen und Root-Element übergeben
+            Scene scene = new Scene(root);
+            
+            //Die Szene der Stage austauschen und 
+            stage.setScene(scene);
+            stage.show();
+
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getLocalizedMessage());
+        }
+    }
+
+}
