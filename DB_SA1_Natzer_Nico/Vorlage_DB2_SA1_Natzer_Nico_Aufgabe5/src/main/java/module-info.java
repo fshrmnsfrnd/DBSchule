d897824ecdb9bafa7b17b74214b8a94f58e4bfc7
fshrmnsfrnd <fshrmnsfrnd@web.de>
2026-03-09 08:59:27 +0100

    removed SA1 Folder

--- DB_SA1_Natzer_Nico/Vorlage_DB2_SA1_Natzer_Nico_Aufgabe5/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ts.blogic {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires java.base;
-
-    opens ts.blogic to javafx.fxml;
-    exports ts.blogic;
-}
